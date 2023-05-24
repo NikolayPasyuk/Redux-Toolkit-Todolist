25c9c99adf9ab059bd65d1ef17da0ea69309de23
NikolayPasyuk <kolyan-1812@mail.ru>
2023-05-24 19:30:27 +0300

    fix: removed property from typing

--- src/features/auth/Login/Login.tsx
+++ src/features/auth/Login/Login.tsx
@@ -19,6 +19,8 @@ import {LoginParamsType} from 'features/auth/auth.api';
 import {ResponseType} from 'common/types';
 import s from './styles.module.css'
 
+type FormikErrorType = Partial<Omit<LoginParamsType, 'captcha'>>
+
 export const Login = () => {
     const {login} = useActions(authThunks)
 
@@ -26,7 +28,7 @@ export const Login = () => {
 
     const formik = useFormik({
         validate: (values) => {
-            const errors: Partial<LoginParamsType> = {};
+            const errors: FormikErrorType = {};
             if (!values.email) {
                 errors.email = 'Email is required';
             } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
