2342bb0c4890435030b3763319b99d7ba90f27ee
NikolayPasyuk <kolyan-1812@mail.ru>
2023-06-01 22:46:27 +0300

    feat: prettier

--- src/features/todolists-list/todolists/Todolist/Tasks/Tasks.tsx
+++ src/features/todolists-list/todolists/Todolist/Tasks/Tasks.tsx
@@ -1,28 +1,28 @@
-import {Task} from 'features/todolists-list/todolists/Todolist/Tasks/Task/Task';
-import React, {FC} from 'react';
-import {TaskStatuses} from 'common/enums';
-import {TodolistDomainType} from 'features/todolists-list/todolists/todolists.reducer';
-import {TaskType} from 'features/todolists-list/tasks/tasks.api';
+import { Task } from "features/todolists-list/todolists/Todolist/Tasks/Task/Task";
+import React, { FC } from "react";
+import { TaskStatuses } from "common/enums";
+import { TodolistDomainType } from "features/todolists-list/todolists/todolists.reducer";
+import { TaskType } from "features/todolists-list/tasks/tasks.api";
 
 type Props = {
-    todolist: TodolistDomainType
-    tasks: TaskType[]
-}
-export const Tasks: FC<Props> = ({todolist, tasks}) => {
+  todolist: TodolistDomainType;
+  tasks: TaskType[];
+};
+export const Tasks: FC<Props> = ({ todolist, tasks }) => {
+  let tasksForTodolist = tasks;
 
-    let tasksForTodolist = tasks
+  if (todolist.filter === "active") {
+    tasksForTodolist = tasks.filter((t) => t.status === TaskStatuses.New);
+  }
+  if (todolist.filter === "completed") {
+    tasksForTodolist = tasks.filter((t) => t.status === TaskStatuses.Completed);
+  }
 
-    if (todolist.filter === 'active') {
-        tasksForTodolist = tasks.filter(t => t.status === TaskStatuses.New)
-    }
-    if (todolist.filter === 'completed') {
-        tasksForTodolist = tasks.filter(t => t.status === TaskStatuses.Completed)
-    }
-
-    return (
-        <>
-            {tasksForTodolist.map(t => <Task key={t.id} task={t}
-                                             todolistId={todolist.id}/>)}
-        </>
-    )
-}
+  return (
+    <>
+      {tasksForTodolist.map((t) => (
+        <Task key={t.id} task={t} todolistId={todolist.id} />
+      ))}
+    </>
+  );
+};
--- src/features/todolists-list/todolists/Todolist/Todolist.tsx
+++ src/features/todolists-list/todolists/Todolist/Todolist.tsx
@@ -1,42 +1,36 @@
-import React, {FC, memo, useEffect} from 'react'
-import {TodolistDomainType} from 'features/todolists-list/todolists/todolists.reducer'
-import {tasksThunks} from 'features/todolists-list/tasks/tasks.reducer';
-import {useActions} from 'common/hooks';
-import {AddItemForm} from 'common/components'
-import {TaskType} from 'features/todolists-list/tasks/tasks.api';
-import {
-    FilterTasksButton
-} from 'features/todolists-list/todolists/Todolist/FilterTasksButton/FilterTasksButton';
-import {Tasks} from 'features/todolists-list/todolists/Todolist/Tasks/Tasks';
-import {
-    TodolistTitle
-} from 'features/todolists-list/todolists/Todolist/TodolistTitle/TodolistTitle';
+import React, { FC, memo, useEffect } from "react";
+import { TodolistDomainType } from "features/todolists-list/todolists/todolists.reducer";
+import { tasksThunks } from "features/todolists-list/tasks/tasks.reducer";
+import { useActions } from "common/hooks";
+import { AddItemForm } from "common/components";
+import { TaskType } from "features/todolists-list/tasks/tasks.api";
+import { FilterTasksButton } from "features/todolists-list/todolists/Todolist/FilterTasksButton/FilterTasksButton";
+import { Tasks } from "features/todolists-list/todolists/Todolist/Tasks/Tasks";
+import { TodolistTitle } from "features/todolists-list/todolists/Todolist/TodolistTitle/TodolistTitle";
 
 type Props = {
-    todolist: TodolistDomainType
-    tasks: TaskType[]
-}
-export const Todolist: FC<Props> = memo(({todolist, tasks}) => {
+  todolist: TodolistDomainType;
+  tasks: TaskType[];
+};
+export const Todolist: FC<Props> = memo(({ todolist, tasks }) => {
+  const { fetchTasks, addTask } = useActions(tasksThunks);
 
-    const {fetchTasks, addTask} = useActions(tasksThunks)
+  useEffect(() => {
+    fetchTasks(todolist.id);
+  }, []);
 
-    useEffect(() => {
-        fetchTasks(todolist.id)
-    }, [])
+  const addTaskCallback = (title: string) => {
+    return addTask({ title, todolistId: todolist.id }).unwrap();
+  };
 
-    const addTaskCallback = (title: string) => {
-        return addTask({title, todolistId: todolist.id}).unwrap()
-    }
-
-    return <div>
-        <TodolistTitle todolist={todolist}/>
-        <AddItemForm addItem={addTaskCallback}
-                     disabled={todolist.entityStatus === 'loading'}/>
-        <Tasks todolist={todolist} tasks={tasks}/>
-        <div style={{paddingTop: '10px'}}>
-            <FilterTasksButton todolist={todolist}/>
-        </div>
+  return (
+    <div>
+      <TodolistTitle todolist={todolist} />
+      <AddItemForm addItem={addTaskCallback} disabled={todolist.entityStatus === "loading"} />
+      <Tasks todolist={todolist} tasks={tasks} />
+      <div style={{ paddingTop: "10px" }}>
+        <FilterTasksButton todolist={todolist} />
+      </div>
     </div>
-})
-
-
+  );
+});
